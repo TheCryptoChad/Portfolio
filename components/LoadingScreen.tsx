3ef88a37d15cb2656c2ac416231f2bce0b74cbf0
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-11-06 11:47:52 -0500

    loading screen

---
+++ components/LoadingScreen.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { Progress } from '@/components/ui/progress';
+
+export default function LoadingScreen() {
+	const [progress, setProgress] = useState<number>(0);
+	const [show, setShow] = useState<boolean>(true);
+
+	useEffect(() => {
+		const interval = setInterval(() => setProgress((prev) => (prev < 100 ? prev + 1 : 100)), 30);
+
+		const timeout = setTimeout(() => setShow(false), 3500);
+
+		return () => {
+			clearInterval(interval);
+			clearTimeout(timeout);
+		};
+	}, []);
+
+	return (
+		<div
+			className={`${!show && 'hidden'} absolute left-0 top-0 z-50 flex h-[100vh] w-[100vw] flex-col items-center justify-around bg-black text-white`}
+		>
+			<Image
+				alt='Logo'
+				height={200}
+				src='/logo.svg'
+				width={200}
+			/>
+			<Progress
+				className='w-1/4'
+				value={progress}
+			/>
+		</div>
+	);
+}
