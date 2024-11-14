7e7afddb4a58cd9af71d2125402230cec2857e79
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-11-14 14:55:44 -0500

    finished seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,15 +24,44 @@ const geistMono: NextFontWithVariable = localFont({
 });
 
 export const metadata: Metadata = {
-	title: 'TheCryptoChad',
-	description: 'TheCryptoChad Portfolio Website',
+	applicationName: 'TheCryptoChad Portfolio Website',
+	title: 'TheCryptoChad - Portfolio Website',
+	description: 'Welcome to TheCryptoChad Portfolio Website. Explore projects, CV, and more about TheCryptoChad.',
+	keywords:
+		'TheCryptoChad, portfolio, projects, CV, web development, blockchain, crypto, cats, frontend, backend, fullstack, developer, engineer, software, engineer, programmer, coding, programming, development, software, web, website, app, application, blockchain, crypto, cryptocurrency, bitcoin, ethereum, solidity, react, nextjs, tailwindcss, typescript, javascript, nodejs, express, mongodb, postgresql, web3, metamask, ethers, hardhat, ipfs, nft, defi, dapp, smart, contract, blockchain, developer, engineer, software, engineer, programmer, coding, programming, development, software, web, website, app, application, blockchain, crypto, cryptocurrency, bitcoin, ethereum, solidity, react, nextjs, tailwindcss, typescript, javascript, nodejs, express, mongodb, postgresql, web3, metamask, ethers, truffle, hardhat, ipfs, nft, defi, dapp, smart, contract',
+	authors: [{ name: 'Adham - TheCryptoChad', url: 'https://www.x.com/@TheCryptoChad_' }],
+	openGraph: {
+		title: 'TheCryptoChad - Portfolio Website',
+		description: 'Welcome to TheCryptoChad Portfolio Website. Explore projects, CV, and more about TheCryptoChad.',
+		url: 'https://www.thecryptochad.com',
+		type: 'website',
+		images: [
+			{
+				url: 'https://www.thecryptochad.com/images/profile.webp',
+				width: 630,
+				height: 630,
+				alt: 'TheCryptoChad Portfolio Website',
+			},
+		],
+	},
+	twitter: {
+		card: 'summary_large_image',
+		site: '@TheCryptoChad_',
+		title: 'TheCryptoChad - Portfolio Website',
+		description: 'Welcome to TheCryptoChad Portfolio Website. Explore projects, CV, and more about TheCryptoChad.',
+		images: 'https://www.thecryptochad.com/images/profile.webp',
+	},
+	robots: 'index, follow',
+	themeColor: '#000000',
+	icons: {
+		icon: '/favicon.ico',
+		apple: '/apple-touch-icon.png',
+	},
 };
 
 export default function RootLayout(props: Props): JSX.Element {
 	return (
-		<html
-			lang='en'
-		>
+		<html>
 			<body className={`${geistSans.variable} ${geistMono.variable} h-[95svh] overflow-hidden bg-black text-white antialiased`}>
 				<Image
 					alt='Background'
