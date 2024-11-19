b6f667829488332db9f28565b27340cf31ea8b1b
Adham - TheCryptoChad <adham_elneser@hotmail.com>
2024-11-19 10:18:02 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,6 @@ import LoadingScreen from '@/components/loading-screen/LoadingScreen';
 import DragNDropCanvas from '@/components/drag-n-drop/DragNDropCanvas';
 
 export default async function Home() {
-	const a = await fetch('http://localhost:3000/api/webhook', {
-		method: 'POST',
-		body: JSON.stringify({
-			type: 'new_coin',
-			message: 'Zerbro was just listed on Binance!',
-			currency: 'ZEREBRO',
-			exchange: 'Binance',
-		}),
-	}).then((res) => res.json());
 	return (
 		<main className='flex size-full justify-end'>
 			<LoadingScreen />
